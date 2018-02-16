e5ec0b7349e55542101ef9a758e2db21dcf340c5
AlexandrBelchev <sashailya42gmail.com>
2018-02-16 20:48:26 +0200

    responsive, mobile menu, scroll

--- app/js/main.js
+++ app/js/main.js
@@ -4,3 +4,21 @@ $(function () {
         $('#menu').toggleClass('menu-active');
     });
 });
+
+$(document).ready(function() {
+    $(window).resize(function(){
+        var windowWidth = $(window).width();
+        if(windowWidth > 769)$("#menu").removeClass("menu-active");
+        if (windowWidth > 769)$("#menu__hamburger").removeClass("active");
+    });
+});
+
+$(function() {
+    $('a').click(function() {
+        var href = $(this).attr("href");
+        $('html, body').animate({
+            scrollTop: $(href).offset().top
+        }, 1000);
+        return false;
+    });
+});
